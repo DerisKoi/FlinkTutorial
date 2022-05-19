16bab0daffaa5387e767bafe6ecb844f2e610cd8
xx0746 <xx0746@qq.com>
2022-05-20 02:09:48 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/chapter05/TransMapTest.java
@@ -0,0 +1,43 @@
+package com.atguigu.chapter05;
+
+/**
+ * Copyright (c) 2020-2030 尚硅谷 All Rights Reserved
+ * <p>
+ * Project:  FlinkTutorial
+ * <p>
+ * Created by  wushengran
+ */
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class TransMapTest {
+    public static void main(String[] args) throws Exception{
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        //有了自定义的source function，调用addSource方法
+        DataStreamSource<Event> stream = env.addSource(new ClickSource());
+
+        // 传入匿名类，实现MapFunction
+        stream.map(new MapFunction<Event, String>() {
+            @Override
+            public String map(Event e) throws Exception {
+                return e.user;
+            }
+        });
+
+        // 传入MapFunction的实现类
+        stream.map(new UserExtractor()).print();
+
+        env.execute();
+    }
+    public static class UserExtractor implements MapFunction<Event, String> {
+        @Override
+        public String map(Event e) throws Exception {
+            return e.user;
+        }
+    }
+}
+
